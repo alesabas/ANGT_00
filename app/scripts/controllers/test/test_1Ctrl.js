337283062f41470f909158e3a53f521678c7838c
alesabas <alejandro_jimenez24@hotmail.com>
2017-07-18 18:04:17 -0500

    inicializando Test_01

--- app/scripts/controllers/test/test_1Ctrl.js
+++ app/scripts/controllers/test/test_1Ctrl.js
@@ -1,7 +1,20 @@
 'use strict';
 
-angular.module('testApp')
-  .controller('Test_1Ctrl', function () {
+angular
+  .module('testApp')
+  .controller('Test_1Ctrl', function (TestFactory) {
+
+    function initData(){
+      TestFactory.ConsTestList().then(function(data){
+        vm.TestList = data.RegTestList;
+        for(var i = 0; i < vm.TestList.length; i++){
+          if(vm.TestList[i].idTest == 1){
+            vm.TestListRest = vm.TestList[i];
+          }
+        }
+      });
+    }
+
     var vm = this;
-    vm.init = [];
+    initData();
   });
