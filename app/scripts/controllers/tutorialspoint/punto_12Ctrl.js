c950c40037b79285c9b614f77f941bbf6c7a5858
alesabas <alejandro_jimenez24@hotmail.com>
2017-08-08 13:39:04 -0500

    Add Punto_12

--- app/scripts/controllers/tutorialspoint/punto_12Ctrl.js
+++ app/scripts/controllers/tutorialspoint/punto_12Ctrl.js
@@ -3,6 +3,18 @@ angular
     .module('AngularApp')
     .controller('Punto_12Ctrl', function(){
 
+        function Reset(){
+            vm.FirstName = "Alejandro";
+            vm.LastName = "Jimenez";
+            vm.Email = "alejs@mail.com";
+        }
+
+        function Submit(){
+            alert('Enviado.');
+        }
+
         var vm = this;
-        
+        vm.Reset = Reset;
+        vm.Submit = Submit;
+
     });
