1955b84fc523bdac715cfa490f307bb2537761bb
alesabas <alejandro_jimenez24@hotmail.com>
2017-08-09 12:41:35 -0500

    Add Capitulo_14

--- app/scripts/controllers/desarrolloweb/modules.js
+++ app/scripts/controllers/desarrolloweb/modules.js
@@ -16,6 +16,13 @@ angular
                 templateUrl: 'views/desarrolloweb/capitulo_13.html',
                 controller: 'Capitulo_13Ctrl',
                 controllerAs: '$ctrl'
+            },
+            {
+                name: 'capitulo.detalle.14',
+                data: {pageTitle: 'DESARROLLOWEB | CAPÍTULO 14'},
+                templateUrl: 'views/desarrolloweb/capitulo_14.html',
+                controller: 'Capitulo_14Ctrl',
+                controllerAs: '$ctrl'
             }
         ];
 
